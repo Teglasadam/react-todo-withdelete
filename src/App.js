ae438ccb39105ae7afda3a1c31eea81b6afd679b
Teglas Adam <ad.teglas@gmail.com>
2024-02-05 14:37:57 +0100

    todo with delete button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [time, setTime] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  function changeTask(event) {
+    setTask(event.target.value);
+  }
+  function changeTime(event) {
+    setTime(event.target.value);
+  }
+
+  function addNewTask() {
+    const newTask = {
+      task: task,
+      time: time,
+    };
+    setTaskList([...taskList, newTask]);
+    setTask("");
+    setTime("");
+  }
+
+  function deleteTask(event) {
+    const idToDelete = event.target.value;
+
+    idToDelete && document.getElementById(idToDelete).remove();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="formContainer">
+        <div className="todo">
+          <label htmlFor="">Add meg a feladatot: </label>
+          <input type="text" value={task} onChange={changeTask} />
+        </div>
+
+        <div className="time">
+          <label htmlFor="">Add meg a ráfordítani kívánt időt: </label>
+          <input type="text" value={time} onChange={changeTime} />
+        </div>
+
+        <button onClick={addNewTask}>Feladat felvétele</button>
+      </div>
+      <div className="todo-list">
+        {taskList.map((task, index) => {
+          return (
+            <div className="item" id={index} onClick={deleteTask}>
+              <p>
+                <strong>Feladat: </strong>
+                {task.task}
+              </p>
+              <p>
+                <strong>Rászánt idő: </strong>
+                {task.time}
+              </p>
+              <button value={index}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
